a5e0e6425a664b5f984d9e7f400e0287d136364e
karthick <karthylock@gmail.com>
2021-02-18 01:36:10 +0530

    added bodyparser

--- calcuator.js
+++ calcuator.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 var app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",function(req,res){
     res.sendFile(__dirname + "/index.html");
-    
+})
+
+app.post("/",function(req,res){
+    console.log(req.body);
+    res.send("Thanks for adding");
 })
 
 app.listen(3000,function(){
