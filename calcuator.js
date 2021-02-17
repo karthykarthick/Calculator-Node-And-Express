ac04894dec75b7014c0eb202530a761b8ac205d3
karthick <karthylock@gmail.com>
2021-02-18 01:55:04 +0530

    added Bmi calculator

--- calcuator.js
+++ calcuator.js
@@ -8,12 +8,23 @@ app.get("/",function(req,res){
     res.sendFile(__dirname + "/index.html");
 })
 
+app.get("/bmicalculator",function(req,res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+})
+
 app.post("/",function(req,res){
     var num1 =Number( req.body.num1);
     var num2 =Number( req.body.num2);
     var result = num1 + num2;
     console.log(result);
     res.send("The result of the calculation is " + result);
+})
+
+app.post("/bmicalculator",function(req,res){
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+    var bmi = weight / (height * height);
+    res.send("your bmi is "+ bmi);
 })
 
 app.listen(3000,function(){
